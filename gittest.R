056384ac56714d6dc984431e5107e7985627af53
helenmpt <helen.thomas.22@ucl.ac.uk>
2025-05-13 15:19:11 +0100

    variable x

--- gittest.R
+++ gittest.R
@@ -1,3 +1,8 @@
 library(usethis)
 
-#if I make some edits to my R file like this one 
+#if I make some edits to my R file like this one and then commit it 
+
+#and then if I make more changes can I see how it changes
+
+#variable 
+x <- 4
